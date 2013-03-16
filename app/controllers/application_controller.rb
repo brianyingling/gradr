17427d66d1b81c2beb65b36cb7bb42d60d7ccc91
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-16 15:31:44 -0400

    sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  private
+  def authenticate
+    @auth = Teacher.find(session[:teacher_id]) if session[:teacher_id].present?
+  end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,13 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    @auth = Teacher.where(:email => params[:email]).first
+    if @auth.present? && @auth.authenticate(params[:password])
+      session[:teacher_id] = @auth.id
+    else
+      session[:teacher_id] = nil
+    end
+  end
 end
