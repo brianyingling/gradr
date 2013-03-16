b297ca47adf69a35d185e3b5a1be65ddb4376599
Brian Yingling <brian_yingling@yahoo.com>
2013-03-16 18:06:10 -0400

    Modified teacher registration for passing tests

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,13 +1,17 @@
 class TeachersController < ApplicationController
+  def index
+  end
+
   def new
     @teacher = Teacher.new
   end
 
   def create
-    @teacher = Teacher.new(params[:teacher])
-    if @teacher.save
-      teacher = Teacher.create
-      teacher.user = @teacher
-    end
+     @teacher = Teacher.create(params[:teacher])
+    # @teacher = Teacher.new(params[:teacher])
+    # if @teacher.save
+    #   teacher = Teacher.create
+    #   teacher.user = @teacher
+    # end
   end
 end
--- spec/features/teachers_spec.rb
+++ spec/features/teachers_spec.rb
@@ -8,15 +8,15 @@ describe 'Teachers' do
     end
   end
 
+  # did not pass
   describe 'GET /teachers/new' do
-    it 'displays the create teacher and cancel buttons', :js => true do
+    it 'displays the create account and cancel buttons', :js => true do
       visit root_path
       click_link('Register')
-      page.should have_link('Cancel')
-      page.should have_link('Create Account')
+      page.should have_button('Cancel')
+      page.should have_button('Create Account')
     end
   end
-end
 
   describe 'JS cancel_teacher_form()' do
     it 'it removes the create account form', :js => true do
@@ -27,27 +27,19 @@ end
     end
   end
 
+  # did not pass
   describe 'POST /teachers' do
-  it 'creates a new teacher', :js => true do
-    visit root_path
-    click_link('Register')
-    fill_in('last_name', :with => 'Socrates')
-    fill_in('Email', :with => 'socrates@gmail.com')
-    save_and_open_page
-  end
-end
-
-describe 'POST /teachers' do
-  it 'creates a new teacher', :js => true do
-    visit root_path
-    click_link('Register')
-    fill_in('last_name', :with => 'Socrates')
-    fill_in('Email', :with => 'socrates@gmail.com')
-    fill_in('teacher_password', :with => 'a')
-    fill_in('teacher_password_confirmation', :with => 'a')
-    click_button('Create Account')
-    expect(Teacher.first.email).to eq 'socrates@gmail.com'
-    page.should_not have_button('Create Account')
-    page.should have_text('You have successfully created an account.')
+    it 'creates a new teacher', :js => true do
+      visit root_path
+      click_link('Register')
+      fill_in('teacher_last_name', :with => 'Socrates')
+      fill_in('teacher_email', :with => 'socrates@gmail.com')
+      fill_in('teacher_password', :with => 'a')
+      fill_in('teacher_password_confirmation', :with => 'a')
+      click_button('Create Account')
+      page.should_not have_button('Create Account')
+      page.should have_text('You have successfully created an account!')
+      expect(Teacher.first.email).to eq 'socrates@gmail.com'
+    end
   end
 end
