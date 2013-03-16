2c6ac37775709347a5ae58af79bb31606d4c0375
Brian Yingling <brian_yingling@yahoo.com>
2013-03-16 12:17:11 -0400

    finished testing student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316151202) do
+ActiveRecord::Schema.define(:version => 20130316160420) do
+
+  create_table "students", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "teachers", :force => true do |t|
     t.string   "email"
