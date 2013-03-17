c084348baac80418e4dfe8a535f515f16fb1d3ae
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-17 19:22:21 -0400

    not much

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+     @student = Student.create(params[:student])
+  end
+end
